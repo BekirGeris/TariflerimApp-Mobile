b12260b719d4c82c3e42c1eda4276cd9a040de8c
BekirGeris <48486272+BekirGeris@users.noreply.github.com>
2021-10-21 14:48:39 +0300

    refactor

--- app/src/main/java/com/begers/tariflerim/roomdb/abstracts/TarifDao.java
+++ app/src/main/java/com/begers/tariflerim/roomdb/abstracts/TarifDao.java
@@ -1,4 +1,4 @@
-package com.begers.tariflerim.roomdb;
+package com.begers.tariflerim.roomdb.abstracts;
 
 import androidx.room.Dao;
 import androidx.room.Delete;
--- app/src/main/java/com/begers/tariflerim/roomdb/concoretes/TarifDatabase.java
+++ app/src/main/java/com/begers/tariflerim/roomdb/concoretes/TarifDatabase.java
@@ -1,9 +1,10 @@
-package com.begers.tariflerim.roomdb;
+package com.begers.tariflerim.roomdb.concoretes;
 
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
 import com.begers.tariflerim.model.Tarif;
+import com.begers.tariflerim.roomdb.abstracts.TarifDao;
 
 @Database(entities = {Tarif.class}, version = 1)
 public abstract class TarifDatabase extends RoomDatabase{
