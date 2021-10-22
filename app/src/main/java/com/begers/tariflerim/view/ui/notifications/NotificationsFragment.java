4e68f52840d17e3894cc1eed43fb8d0fc7793508
BekirGeris <48486272+BekirGeris@users.noreply.github.com>
2021-10-22 13:38:39 +0300

    +pp şeçmek için galeriye gidiş

--- app/src/main/java/com/begers/tariflerim/view/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/begers/tariflerim/view/ui/notifications/NotificationsFragment.java
@@ -1,19 +1,139 @@
 package com.begers.tariflerim.view.ui.notifications;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.ImageDecoder;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.begers.tariflerim.R;
+import com.begers.tariflerim.databinding.FragmentNotificationsBinding;
+import com.google.android.material.snackbar.Snackbar;
+
+import static android.app.Activity.RESULT_OK;
 
 public class NotificationsFragment extends Fragment {
 
+    FragmentNotificationsBinding binding;
+
+    ActivityResultLauncher<Intent> activityResultLauncher;  //galeriye gitmek için kullanılır
+    ActivityResultLauncher<String> permissionLauncher;  //izin almak için kullanılır.
+    Bitmap selectedImage;
+
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_notifications, container, false);
+        binding = FragmentNotificationsBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        registerLauncher();
+        binding.circleImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectedImage(view);
+            }
+        });
+    }
+
+    public void selectedImage(View view){
+        if(ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), Manifest.permission.READ_EXTERNAL_STORAGE)){ //izin verilmedi izinin istenilme sebebi gösterilsin mi
+                //izin ikinci kez verilmemiş
+                Snackbar.make(view, "Galeriye gitmek için izniniz gerekiyor.", Snackbar.LENGTH_INDEFINITE).setAction("İzin ver", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE);
+                    }
+                }).show();
+            }else {
+                //izin verilmemiş
+                permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE);
+            }
+        }else{
+            //izin verilmiş
+            Intent intentToGallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            activityResultLauncher.launch(intentToGallery);
+        }
+    }
+
+    private void registerLauncher(){  //tanımlamalar yapılacak
+
+        activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                if (result.getResultCode() == RESULT_OK){
+                    Intent intentFromResult = result.getData();
+                    if (intentFromResult != null){
+                        Uri imageData = intentFromResult.getData(); //kullanıcının seçtiği resmin kaynağını verir.
+                        binding.circleImageView.setImageURI(imageData);
+
+                        try{
+                            if (Build.VERSION.SDK_INT >= 28) {
+                                ImageDecoder.Source source = ImageDecoder.createSource(getActivity().getContentResolver(), imageData);
+                                selectedImage = ImageDecoder.decodeBitmap(source);
+                            }else {
+                                selectedImage = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), imageData);
+                            }
+                            //binding.imageView.setImageBitmap(selectedImage);
+                        }catch (Exception e){
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        });
+
+        permissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), new ActivityResultCallback<Boolean>() {
+            @Override
+            public void onActivityResult(Boolean result) {
+                if (result){
+                    //izin verildi
+                    Intent intentToGallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    activityResultLauncher.launch(intentToGallery);
+                }else{
+                    //izin verilmedi
+                    Toast.makeText(getActivity(), "İzin verilmedi", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+    }
+
+    public Bitmap makeSmallerImage(Bitmap image, int maximumSize) {
+
+        int width = image.getWidth();
+        int height = image.getHeight();
+
+        float bitmapRatio = (float) width / (float) height;
+
+        if (bitmapRatio > 1) {
+            width = maximumSize;
+            height = (int) (width / bitmapRatio);
+        } else {
+            height = maximumSize;
+            width = (int) (height * bitmapRatio);
+        }
+
+        return Bitmap.createScaledBitmap(image,width,height,true);
+    }
 }
