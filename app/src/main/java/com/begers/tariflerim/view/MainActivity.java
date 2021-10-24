432af3f8b31089d34d312cf6e122181880df5d56
bekirgeris <bekirgeris@marun.edu.tr>
2021-10-24 22:54:30 +0300

    +3 dot menu +log out item

--- app/src/main/java/com/begers/tariflerim/view/MainActivity.java
+++ app/src/main/java/com/begers/tariflerim/view/MainActivity.java
@@ -1,15 +1,24 @@
 package com.begers.tariflerim.view;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.begers.tariflerim.R;
+import com.begers.tariflerim.utiles.SingletonUser;
+import com.begers.tariflerim.view.ui.Login.LoginActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+
+import io.reactivex.rxjava3.core.Single;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,7 +35,23 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
-
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.top_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.log_out){
+            SingletonUser singletonUser = SingletonUser.getInstance();
+            singletonUser.clearUser();
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
